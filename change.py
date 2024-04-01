2b3c3aa6d9234410c0bfa69fa474ce0d47d2b410
LucianoRenkine <162723184+LucianoRenkine@users.noreply.github.com>
2024-04-01 13:20:26 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,3 +1,12 @@
 def change():
     expense = 23.75
     money = 100
+    vueltop = int(money - expense)
+    vueltoc = (money-expense)*100-(vueltop)*100
+    print(f"Ingresar gasto:\n{expense}")
+    print(f"Dinero recibido\n{money}")
+    print(f"\nVuelto")
+    print(f"\nPesos:\n{vueltop}")
+    print(f"Centavos:\n{int(vueltoc)}")
+
+change()
